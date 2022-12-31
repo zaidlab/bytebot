2f9555c18876221b79e80c16eba41d3bb9728473
Zaid <53094337+zaidlab@users.noreply.github.com>
2022-12-31 10:11:40 -0500

    Re-initialize, thank you pulsar
    Huge credits to pulsar.

---
+++ src/functions/handlers/handleCommands.js
@@ -0,0 +1,43 @@
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const fs = require("fs");
+
+module.exports = (client) => {
+  client.handleCommands = async () => {
+    const commandFolders = fs.readdirSync("./src/commands");
+    for (const folder of commandFolders) {
+      const commandFiles = fs
+        .readdirSync(`./src/commands/${folder}`)
+        .filter((file) => file.endsWith(".js"));
+
+      const { commands, commandArray } = client;
+      for (const file of commandFiles) {
+        const command = require(`../../commands/${folder}/${file}`);
+        commands.set(command.data.name, command);
+        commandArray.push(command.data.toJSON());
+        //console.log('Command:  ${command.data.name} has been passed through the handler');
+      }
+    }
+
+    //right click bot and choose copy id
+    const clientId = "1058465724819050578"; //change for your bot
+    //used if you only want the bot to be used in a specific server
+    //right click on server and choose copy id
+    const guildId = "1058465234521686036"; //change for your server
+    const rest = new REST({ version: "9" }).setToken(process.env.token);
+    try {
+      console.log("Started refreshing application (/) commands.");
+
+      //use applicationGuildCommands(clientId,guildId) if you only want the bot in a specific server
+      //otherwise use applicationCommands(guildId)
+      //if applicationCommands() is used, commands might take time to update for all servers
+      await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+        body: client.commandArray,
+      });
+
+      console.log("Successfully reloaded application (/) commands.");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
