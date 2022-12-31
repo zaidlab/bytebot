4f46f9a7ba13c0614a4cff1e654b5a63e5c0a024
Zaid <53094337+zaidlab@users.noreply.github.com>
2022-12-31 10:10:07 -0500

    re-uploading

--- events/ready.js
+++
@@ -1,8 +0,0 @@
-// When the client is ready, run this code (only once)
-module.exports = {
-	name: 'ready',
-	once: true,
-	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-	},
-};
